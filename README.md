8075f0c623bc8f40618c444c8102229db30e6f8d
Jerry M <developer.jerrymannel@gmail.com>
2020-01-20 10:47:34 +0530

    README updated

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # mongoose-crud
-mongoose-crud middleware for Express
+CRUN middleware for mongoose
+
+# Quick start
+
+Define your schema and create a new mongose-crud
+
+```js
+var definition = {
+	"_id": { "type": String },
+	"name": { "type": String },
+	"description": { "type": String },
+	"age" : {"type" : Number}
+}
+
+var schema = Mongoose.Schema(definition)
+var modelName = "foobar"
+var options = {
+	collectionName: "foobar",
+	defaultFilter: {
+		"age": {"$gte": 10}
+	}
+}
+
+var fooCrud = new MongooseCRUD(modelName, schema, options)
+```
+
+Add the middleware to express
+
+```js
+var app = express()
+app.use(express.json())
+
+app.post("/foo", fooCrud.create)
+app.get("/foo", fooCrud.index)
+app.get("/foo/bulkShow", fooCrud.bulkShow)
+app.put("/foo/bulkUpdate", fooCrud.bulkUpdate)
+app.delete("/foo/bulkDelete", fooCrud.bulkDestroy)
+app.get("/foo/:id", fooCrud.show)
+app.put("/foo/:id", fooCrud.update)
+app.delete("/foo/:id", fooCrud.destroy)
+
+app.listen(8080)
+```
+
+# Table of contents
+
+1. [Quick start](#Quick-start)
+2. [Constructor](#Constructor)
+
+# Constructor
+
+`var fooCrud = new MongooseCRUD(modelName, schema, options)`
+
+The constructor takes 3 values, 
+
+* **modelName**(_Required_): Name of the mongoosle model.
+* **schema**(_Required_): The schema object returned by `Mongoose.Schema()`
+* **options*(_Optional_): An optional options object. This has two properties.
+	* _collectionName_: [By default Mongoose uses the pluralized model name as the collection name](https://mongoosejs.com/docs/guide.html#collection). If you wish to override this, then provide your custom collection name here.
+	* _defaultFilter_: A default filter to be applied to all `GET` calls.
--- example/server.js
+++ example/server.js
@@ -3,29 +3,29 @@ const express = require("express")
 const port = process.env.PORT || 8080
 
 var Mongoose = require("mongoose")
-var MC = require("../")
-// var tests = require("./index.js")
+var MongooseCRUD = require("../")
 
 var definition = {
-	"_id": { "type": String },
-	"name": { "type": String },
-	"description": { "type": String },
-	"age" : {"type" : Number}
+    "_id": { "type": String },
+    "name": { "type": String },
+    "description": { "type": String },
+    "age": { "type": Number }
 }
 
 var schema = Mongoose.Schema(definition)
 var modelName = "foobar"
 var options = {
-	collectionName: "foobar",
-	defaultFilter: {
-		"age": {"$gte": 10}
-	}
+    collectionName: "foobar",
+    defaultFilter: {
+        "age": { "$gte": 10 }
+    }
 }
 
-var testCrud = new MC(modelName, schema, options)
+var fooCrud = new MongooseCRUD(modelName, schema, options)
 
-Mongoose.connect("mongodb://localhost:27017/mongoose-crudder-test", { useNewUrlParser: true,
-	useUnifiedTopology: true
+Mongoose.connect("mongodb://localhost:27017/mongoose-crudder-test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }, err => {
     if (err) {
         console.error(err)
@@ -36,24 +36,22 @@ Mongoose.connect("mongodb://localhost:27017/mongoose-crudder-test", { useNewUrlP
 })
 
 function init() {
-	var app = express()
-	app.use(express.json())
+    var app = express()
+    app.use(express.json())
 
-	app.post("/foo", testCrud.create)
-	app.get("/foo", testCrud.index)
-	app.get("/foo/bulkShow", testCrud.bulkShow)
-	app.put("/foo/bulkUpdate", testCrud.bulkUpdate)
-	app.delete("/foo/bulkDelete", testCrud.bulkDestroy)
-	app.get("/foo/:id", testCrud.show)
-	app.put("/foo/:id", testCrud.update)
-	app.delete("/foo/:id", testCrud.destroy)
+    app.post("/foo", fooCrud.create)
+    app.get("/foo", fooCrud.index)
+    app.get("/foo/bulkShow", fooCrud.bulkShow)
+    app.put("/foo/bulkUpdate", fooCrud.bulkUpdate)
+    app.delete("/foo/bulkDelete", fooCrud.bulkDestroy)
+    app.get("/foo/:id", fooCrud.show)
+    app.put("/foo/:id", fooCrud.update)
+    app.delete("/foo/:id", fooCrud.destroy)
 
-	app.listen(port, (err) => {
-    if (!err) {
-        console.log("Server started on port " + port)
-        // require("./index.js")().then(() => process.exit())
-    } else
-        console.error(err)
-	})
+    app.listen(port, (err) => {
+        if (!err) {
+            console.log("Server started on port " + port)
+        } else
+            console.error(err)
+    })
 }
-
